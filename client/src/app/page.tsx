a809cac38e3ab8e1f35f5c668775fac7a5222a4b
Ethan Zhou <ezhou0716@gmail.com>
2026-04-18 04:16:13 -0700

    Mount InteriorCaption on interior page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,11 +3,13 @@
 import dynamic from "next/dynamic";
 
 const ISSInteriorScene = dynamic(() => import("@/components/ISSInteriorScene"), { ssr: false });
+const InteriorCaption = dynamic(() => import("@/components/InteriorCaption"), { ssr: false });
 
 export default function Home() {
   return (
     <div className="h-screen w-screen relative">
       <ISSInteriorScene />
+      <InteriorCaption />
     </div>
   );
 }
